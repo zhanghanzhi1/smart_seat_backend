7f2bbe0e11fe974acb0b7cd094d41bd14cfa0748
zhanghanzhi1 <hanzhi.zhang@my.jcu.edu.au>
2025-10-16 20:03:06 +0800

    update

--- smart-seat-backend/index.js
+++ smart-seat-backend/index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors({
-  origin: 'https://smart-seat-frontend-fc6d.vercel.app/', // 前端开发环境地址（根据实际前端端口调整）
+  origin: 'https://smart-seat-frontend-fc6d.vercel.app', // 前端开发环境地址（根据实际前端端口调整）
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的请求方法（前端有用到POST）
   allowedHeaders: ['Content-Type'], // 允许的请求头（前端未使用自定义头部，默认Content-Type即可）
   credentials: true // 若前端请求需要携带cookie等凭证，需设为true（根据你的需求选择）
